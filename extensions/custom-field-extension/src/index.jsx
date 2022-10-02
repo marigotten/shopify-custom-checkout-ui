304d1ec79539229ffd9427ae82cf1c9a118265c8
marigotten <47206030+marigotten@users.noreply.github.com>
2022-10-02 15:56:37 +0900

    💎  Create a metafield function

--- extensions/custom-field-extension/src/index.jsx
+++ extensions/custom-field-extension/src/index.jsx
@@ -10,17 +10,31 @@ import { useState } from 'react';
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
+  const METAFIELD_NAMESPACE = 'RESIDENT_ID_APP';
+  const METAFIELD_KEY = 'resident_id';
+
   const [error, setError] = useState(false);
-  const [residentID, setResidentID] = useState('');
+  const updateMetafield = useApplyMetafieldsChange();
+
+  const residentIdState = useMetafield({
+    namespace: 'METAFIELD_NAMESPACE',
+    key: METAFIELD_KEY,
+  })
 
   const handleFieldChange = (value) => {
-    setResidentID(value);
+    updateMetafield ({
+      type: 'updateMetafield',
+      namespace: 'METAFIELD_NAMESPACE',
+      key: METAFIELD_KEY,
+      valueType: 'string',
+      value: value
+    })
   };
 
   return ( 
     <TextField
-      label='resident ID'
-      value={residentID}
+      label='Resident ID'
+      value={residentIdState?.value}
       error={error? 'Please provide a valid ID': false}
       onChange={handleFieldChange}
     />
--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -1,2 +1,5 @@
 export { ProductsCard } from "./ProductsCard";
 export * from "./providers";
+
+const METAFIELD_NAMESPACE = 'RESIDENT_ID_APP';
+const METAFIELD_KEY = 'resident_id';
